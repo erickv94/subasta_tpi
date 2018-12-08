89bee1a8c7b3917cd65b7c1a4be6df2149cae410
Eduardo Reye <reduardo.dev21@gmail.com>
2018-12-08 10:44:44 -0600

    Reset Password

---
+++ resources/views/users/reset-password.blade.php
@@ -0,0 +1,36 @@
+<section>
+    <div class="container p-5">
+        <div class="row m-5 ">
+            <div class="col-md-10">
+
+                <div class="card text-center">
+                    <div class="card-header">
+                        Cambio de contraseña
+                    </div>
+                    <div class="card-body">
+                            <form>
+                                    <div class="form-group row">
+                                      <label for="colFormLabelSm" class="col-sm-4 col-form-label col-form-label-sm">Contraseña</label>
+                                      <div class="col-sm-8">
+                                        <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="Nueva contraseña">
+                                      </div>
+                                    </div>
+                                    <div class="form-group row">
+                                            <label for="colFormLabelSm" class="col-sm-4 col-form-label col-form-label-sm">Reescribir contraseña</label>
+                                            <div class="col-sm-8">
+                                              <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="Confirmar contraseña ">
+                                            </div>
+                                          </div>
+                                          <button type="button" class="btn btn-primary btn-sm">Guardar</button>
+                                   
+                                  </form>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</section>
